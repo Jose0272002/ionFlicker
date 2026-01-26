5cca657a990e89d98733afcfadfd5a6cc434294c
user <j.emilio0272002@gmail.com>
2026-01-26 02:35:29 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionFlicker',
+  webDir: 'www'
+};
+
+export default config;
